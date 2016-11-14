264f4e0140cdc91b2c9528a51d03df8098e1fcf4
boris <chris.bohnert@eduphoric.com>
2016-11-13 21:48:00 -0600

    initial checkin

--- src/app.js
+++ src/app.js
@@ -1,8 +1,27 @@
+import {
+    container,
+    dependencies
+} from 'needlepoint';
+import {
+    Test
+} from './test';
+
+@dependencies(Test)
 class Person {
+    constructor(Test) {
+        this.test = Test;
+    }
     sayHi() {
         console.log("hello");
+        this.test.sayLo();
     }
 }
 
-let per = new Person();
-per.sayHi();
+let per = container.resolve(Person);
+var page = ({
+    per
+}) => {
+    return `<h2>Person Says</h2>
+    ${per.sayHi()}`;
+}
+console.log(page(per));
